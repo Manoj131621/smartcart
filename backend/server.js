1fe30030a8e299100f36b8b09cc9f5a7ae22d8c2
Manojkumar <manojprfp@gmail.com>
2026-06-15 14:44:08 +0530

    feat: add user authentication with JWT and bcrypt

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 connectDB();
@@ -14,6 +15,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("SmartCart API is running");
 });
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
