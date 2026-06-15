81fa951da4e3d5455fdcc87ef4e484d05ae68cbe
Manojkumar <manojprfp@gmail.com>
2026-06-15 18:48:03 +0530

    feat: add cart model with item subdocuments

---
+++ backend/models/cartModel.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const cartItemSchema = mongoose.Schema(
+  {
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    qty: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    _id: false,
+  }
+);
+
+const cartSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    items: [cartItemSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Cart = mongoose.model("Cart", cartSchema);
+
+module.exports = Cart;
